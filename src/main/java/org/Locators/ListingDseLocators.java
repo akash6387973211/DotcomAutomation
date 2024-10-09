b448d53f0646cb95fbcb5a815965d41588c68452
akash <tapa3356@gmail.com>
2024-10-09 16:51:24 +0530

    checking

--- src/main/java/org/Locators/ListingDseLocators.java
+++ src/main/java/org/Locators/ListingDseLocators.java
@@ -4,5 +4,5 @@ import org.openqa.selenium.By;
 
 public class ListingDseLocators {
     public By ZonesLink = By.xpath("//*[@class='topLocalitiesTableBody']//li//div");
-    public By LocalityLink = By.xpath("//*[@class='tableBaseLocality']//td//a");
+//    public By LocalityLink = By.xpath("//*[@class='tableBaseLocality']//td//a");/
 }
